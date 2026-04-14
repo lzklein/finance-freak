34ae7d82019f6c7b789e58b6b959836a684eaba7
Louis Klein <louisklein71@gmail.com>
2026-04-14 01:29:08 -0700

    basic login stuff

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    const loginSubmission = {
+      username: username,
+      password: password
+    }
+
+    console.log(loginSubmission);
+  }
+
   return (
-    <div>Login</div>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='Email or Username' value={username} onChange={(e)=>{setUsername(e.target.value)}}></input>
+        <input type="password" placeholder='password' value={password} onChange={(e)=>{setPassword(e.target.value)}}></input>
+        <button type="submit">Login</button>
+      </form>
+      <a onClick={() => navigate('/register')}>Don't have an account? Click here to register</a>
+    </div>
   )
 }
 
