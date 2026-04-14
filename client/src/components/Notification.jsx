abe852ce82e9e5837f1d41d1a1cf10f12f1fb213
Louis Klein <louisklein71@gmail.com>
2026-04-13 21:14:07 -0700

    Create Notification.jsx

---
+++ client/src/components/Notification.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Alert from './Alert';
+import Inbox from './Inbox';
+
+const Notification = () => {
+  return (
+    <div>
+        <Alert/>
+        <Inbox/>
+    </div>
+  )
+}
+
+export default Notification
