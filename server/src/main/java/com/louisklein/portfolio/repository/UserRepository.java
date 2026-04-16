39be55bd662bda601afd4caeac2c79e506152833
Louis Klein <louisklein71@gmail.com>
2026-04-16 16:17:09 -0700

    auto delete repo

--- server/src/main/java/com/louisklein/portfolio/repository/UserRepository.java
+++ server/src/main/java/com/louisklein/portfolio/repository/UserRepository.java
@@ -2,10 +2,13 @@ package com.louisklein.portfolio.repository;
 
 import com.louisklein.portfolio.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Repository;
 
+import java.time.OffsetDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -20,6 +23,11 @@ public interface UserRepository extends JpaRepository<User, UUID> {
 
     @Query("SELECT u FROM User u WHERE u.email = :input OR u.username = :input")
     Optional<User> findByEmailOrUsername(@Param("input") String input);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM User u WHERE u.verified = false AND u.verificationTokenExpiresAt < :now")
+    int deleteExpiredUnverifiedUsers(@Param("now") OffsetDateTime now);
 
     Optional<User> findByVerificationToken(String token);
 }
