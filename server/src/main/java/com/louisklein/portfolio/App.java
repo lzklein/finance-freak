80bb968de7eb8faeb196589a9a42c99244fbb58b
Louis Klein <louisklein71@gmail.com>
2026-04-18 07:41:22 -0700

    caching

--- server/src/main/java/com/louisklein/portfolio/App.java
+++ server/src/main/java/com/louisklein/portfolio/App.java
@@ -2,10 +2,12 @@ package com.louisklein.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
