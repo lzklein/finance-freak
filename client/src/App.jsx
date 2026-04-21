bb94997ea17bdfd3135895656d718bab22fe8a31
Louis Klein <louisklein71@gmail.com>
2026-04-21 15:54:04 -0700

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ import Dashboard from './components/Dashboard';
 import Confirmation from './components/Confirmation';
 import Login from './components/Login';
 import Search from './components/Search';
+import AssetPage from './components/AssetPage'
 
 function App() {
   return (
@@ -20,13 +21,14 @@ function App() {
       <div className="App">
         <Header/>
           <Routes>
-            <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/search" element={<Search />} />
             <Route path="/register" element={<Register />} />
             <Route path="/confirmation" element={<Confirmation />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/inbox" element={<Inbox />} />
+            <Route path="/asset/:id" element={<AssetPage />} />
+            <Route path="/" element={<Home />} />
           </Routes>
         <Footer/>
       </div>
